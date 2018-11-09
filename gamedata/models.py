8eff130747e49271a9ba20b256f97f7ff8b34ff6
golony6449 <ab6449@gmail.com>
2018-11-09 21:29:04 +0900

    DB 설계 (gamedata)

--- gamedata/models.py
+++ gamedata/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Gamedata(models.Model):
+    index = models.AutoField(primary_key=True)
+    game_name = models.CharField(max_length=50)
+    score_type = models.CharField(max_length=10)
+    api_key = models.CharField(max_length=50)
+    admin_name = models.CharField(max_length=10)
+
+
+class Ladder(models.Model):
+    index = models.AutoField(primary_key=True)
+    game_index = models.ForeignKey('Gamedata', on_delete=models.CASCADE)
+    score = models.IntegerField()
+    player_id = models.CharField(max_length=10)
