57173908ae10d159b2c5557da4d05e254584938c
golony6449 <ab6449@gmail.com>
2018-11-16 23:46:30 +0900

    사용자 목록 테이블을 참조하도록 DB 스키마 수정

--- gamedata/models.py
+++ gamedata/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
 
 
 class Gamedata(models.Model):
@@ -6,11 +8,12 @@ class Gamedata(models.Model):
     game_name = models.CharField(max_length=50)
     score_type = models.CharField(max_length=10)
     api_key = models.CharField(max_length=50)
-    admin_name = models.CharField(max_length=10)
+    admin_name = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 
 class Ladder(models.Model):
     index = models.AutoField(primary_key=True)
     game_index = models.ForeignKey('Gamedata', on_delete=models.CASCADE)
     score = models.IntegerField()
-    player_id = models.CharField(max_length=10)
+    # player_id = models.CharField(max_length=10)
+    player_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
